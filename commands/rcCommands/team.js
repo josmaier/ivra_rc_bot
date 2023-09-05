792070188e7630a691f1fa77a5a6c8d02af1a18d
josch <joscha.maier@live.de>
2023-09-05 13:01:40 +0200

    Fixed Spelling

--- commands/rcCommands/team.js
+++ commands/rcCommands/team.js
@@ -7,12 +7,12 @@ module.exports = {
         .addChannelOption(option => 
             option
             .setName('channel')
-            .setDescription('Name of the channel you want to send the message too')
+            .setDescription('Name of the channel you want to send the message to')
             .setRequired(true)
         ).addStringOption(option =>
             option
             .setName('message')
-            .setDescription('The message you want to send')
+            .setDescription('The message you want to sent')
             .setRequired(true)
         ),
         async execute(interaction){
@@ -21,10 +21,10 @@ module.exports = {
             const message = interaction.options.getString('message');
             const eEmbed = new EmbedBuilder()
 		    .setColor('#E67E22')
-		    .setTitle('Race control send a message')
+		    .setTitle('Race control sent a message')
 		    .setDescription(message)
             .setTimestamp();
             await channel.send({embeds: [eEmbed]});
-            await interaction.editReply({ content: 'Your message has been send'});
+            await interaction.editReply({ content: 'Your message has been sent'});
         }
 }
